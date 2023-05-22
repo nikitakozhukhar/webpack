a6f0ca5ef0a7624e6442bed14d8f9ecfb3262d7d
NikitaKx9 <nikitakx9@gmail.com>
2023-05-22 05:11:19 +0300

    webpack v1

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  modules: {
+    rules: [
+
+    ],
+    
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'index.html'),
+        filename: 'index.html',
+      }),
+    ],
+  }
+  
+
+}
